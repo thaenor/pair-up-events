bda35d3617453db3a0a6a365849cf81e790e396e
thaenor <3703930+thaenor@users.noreply.github.com>
2025-10-17 19:05:34 +0100

    chore: import Sentry library for error tracking integration

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import './index.css'
 
 import { createRoot } from 'react-dom/client'
 
+import './lib/sentry'
+
 import App from './App.tsx'
 
 createRoot(document.getElementById("root")!).render(<App />);
